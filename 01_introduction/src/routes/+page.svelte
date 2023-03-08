6eb84efb17f372d2870a3a264e949d7b5f9d3380
arnoldknott <knott@dtu.dk>
2023-03-08 19:48:43 +0100

    feat: adds introduction from tutorial

---
+++ 01_introduction/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Nested from "../components/Nested.svelte";
+
+	const world = 'world';
+	let src = '/tutorial/image.gif';
+	const name = 'man';
+</script>
+
+<h1>Hello {world.toUpperCase()}</h1>
+<!-- <img src={src} alt="A {name} dances."> -->
+<img {src} alt="A {name} dances." />
+
+<p>This is a paragraph.</p>
+
+<Nested />
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
