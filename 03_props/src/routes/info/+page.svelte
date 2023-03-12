18e24e7cc30fbba13b6e7a2c1b2293e23cc756b1
arnoldknott <knott@dtu.dk>
2023-03-12 20:48:20 +0100

    feat: adds props lesson to svelte tutorial

---
+++ 03_props/src/routes/info/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Info from "../../components/Info.svelte";
+    const pkg = {
+        name: 'svelte',
+        version: '3',
+        speed: 'blazing',
+        website: 'https://svelte.dev'
+    }
+</script>
+
+<Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website} />
+<Info {...pkg} />
